cc2aa764292db5217ecf3411741907065494b6a5
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-08 02:06:17 +0300

    Extract the `handlers.FileHandler.SaveFile()` method

--- gateways/handlers/file_handler.go
+++ gateways/handlers/file_handler.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
+	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"sort"
+	"strings"
 
 	"github.com/thewizardplusplus/go-upload-progress/entities"
+)
+
+const (
+	randomSuffixByteCount = 4
 )
 
 type FileHandler struct{}
@@ -45,4 +54,66 @@ func (h FileHandler) GetFiles(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(bytes) // nolint: errcheck
+}
+
+func (h FileHandler) SaveFile(w http.ResponseWriter, r *http.Request) {
+	sourceFile, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer sourceFile.Close()
+
+	uniqueFilename, err := generateUniqueFilename("./files", fileHeader.Filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	targetFile, err := os.Create(uniqueFilename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer targetFile.Close()
+
+	if _, err := io.Copy(targetFile, sourceFile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func generateUniqueFilename(baseDir string, originalFilename string) (string, error) {
+	files, err := os.ReadDir(baseDir)
+	if err != nil {
+		return "", err
+	}
+
+	filenameSet := make(map[string]struct{})
+	for _, file := range files {
+		if !file.IsDir() {
+			filenameSet[file.Name()] = struct{}{}
+		}
+	}
+
+	uniqueFilename := originalFilename
+	for {
+		if _, exists := filenameSet[uniqueFilename]; !exists {
+			break
+		}
+
+		randomSuffixBytes := make([]byte, randomSuffixByteCount)
+		if _, err := rand.Read(randomSuffixBytes); err != nil {
+			return "", err
+		}
+
+		extension := filepath.Ext(uniqueFilename)
+		uniqueFilename = strings.TrimSuffix(uniqueFilename, extension) +
+			hex.EncodeToString(randomSuffixBytes) +
+			extension
+	}
+
+	return filepath.Join(baseDir, uniqueFilename), nil
 }
