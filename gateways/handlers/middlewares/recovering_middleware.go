4466f99c0b51b7a3b9e927e37db45f9819067c3e
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-09 23:34:01 +0300

    Add the `middlewares.RecoveringMiddleware()` function

---
+++ gateways/handlers/middlewares/recovering_middleware.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/thewizardplusplus/go-upload-progress-backend/gateways/handlers"
+)
+
+func RecoveringMiddleware(logger handlers.Logger) Middleware {
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Print(err)
+					http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+				}
+			}()
+
+			handler.ServeHTTP(w, r)
+		})
+	}
+}
