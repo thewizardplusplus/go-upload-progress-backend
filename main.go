5e93815bd15e36f454aca14560a4aaa6e2921360
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-09 23:42:46 +0300

    Use the middlewares in the main code

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/thewizardplusplus/go-upload-progress/gateways/handlers"
+	"github.com/thewizardplusplus/go-upload-progress/gateways/handlers/middlewares"
 	"github.com/thewizardplusplus/go-upload-progress/gateways/writablefs"
 	"github.com/thewizardplusplus/go-upload-progress/usecases"
 )
@@ -15,19 +16,25 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	logger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds)
-	fileHandler := handlers.FileHandler{
+	errorLogger := log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	infoLogger := log.New(os.Stderr, "[INFO] ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+
+	mux := http.NewServeMux()
+	mux.Handle("/api/v1/files", handlers.FileHandler{
 		FileUsecase: usecases.FileUsecase{
 			WritableFS: writablefs.NewWritableFS("./files"),
 		},
-		Logger: logger,
-	}
+		Logger: errorLogger,
+	})
+	mux.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files"))))
+	mux.Handle("/", http.FileServer(http.Dir("./public")))
 
-	http.Handle("/api/v1/files", fileHandler)
-	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files"))))
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	wrappedMux := middlewares.ApplyMiddlewares(mux, []middlewares.Middleware{
+		middlewares.LoggingMiddleware(infoLogger),
+		middlewares.RecoveringMiddleware(errorLogger),
+	})
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		logger.Fatal(err)
+	if err := http.ListenAndServe(":8080", wrappedMux); err != nil {
+		errorLogger.Fatal(err)
 	}
 }
